ad92b7d2c2ed20f5125236b81df7ff0adfd41893
Luca Sammarco <luca.sammarco7@gmail.com>
2025-06-12 16:53:06 +0200

    Project Set up - Created Task migration

---
+++ app/Models/Task.php
@@ -0,0 +1,32 @@
+<?php
+
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Task extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'description',
+        'due_date',
+        'status',
+        'created_by',
+        'assigned_to',
+    ];
+
+    public function creator()
+    {
+        return $this->belongsTo(User::class, 'created_by');
+    }
+
+    public function assignee()
+    {
+        return $this->belongsTo(User::class, 'assigned_to');
+    }
+}
